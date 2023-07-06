668722cd1479af3fe8e660f2d238179d54a337d5
kdw <kdw@gmail.com>
2023-07-06 21:43:58 +0900

    [김도원] - lifescycle, database 예제 추가

---
+++ src/main/java/com/mylb/spring/ex/database/UsedGoodsController.java
@@ -0,0 +1,30 @@
+package com.mylb.spring.ex.database;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mylb.spring.ex.database.domain.UsedGoods;
+import com.mylb.spring.ex.database.service.UsedGoodsService;
+
+// request, response 처리 담당
+@Controller
+public class UsedGoodsController {
+	
+	@Autowired
+	private UsedGoodsService usedGoodsService;
+	
+	// used_goods 목록 조회
+	@RequestMapping("/db/usedgoods/list")
+	@ResponseBody
+	public List<UsedGoods> usedGoodsList() {
+		
+		List<UsedGoods> usedGoodsList = usedGoodsService.getUsedGoodsList();
+		
+		return usedGoodsList;
+		
+	}
+}
---
+++ src/main/java/com/mylb/spring/ex/database/service/UsedGoodsService.java
@@ -0,0 +1,25 @@
+package com.mylb.spring.ex.database.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mylb.spring.ex.database.domain.UsedGoods;
+import com.mylb.spring.ex.database.repository.UsedGoodsRepository;
+
+// 로직을 담당(business)
+@Service
+public class UsedGoodsService {
+	
+	@Autowired
+	private UsedGoodsRepository usedGoodsRepository;
+
+	// used_goods 리스트 조회 기능
+	public List<UsedGoods> getUsedGoodsList() {
+		List<UsedGoods> usedGoodsList = usedGoodsRepository.selectUsedGoodsList();
+		
+		return usedGoodsList;
+	}
+	
+}
