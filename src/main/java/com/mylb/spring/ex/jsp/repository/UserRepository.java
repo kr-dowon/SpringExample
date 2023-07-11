93f086b088d4d306d925135300bdc1a68da848d6
DESKTOP-GGD42CM\USER <kr.dowon@gmail.com>
2023-07-11 21:47:29 +0900

    [김도원] - jsp 예제 풀이 추가

---
+++ src/main/java/com/mylb/spring/ex/jsp/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.mylb.spring.ex.jsp.repository;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.mylb.spring.ex.jsp.domain.User;
+
+@Repository
+public interface UserRepository {
+	
+	// 사용자 정보 전달받고 insert
+	public int insertUser(
+			@Param("name") String name
+			, @Param("birthday") String birthday
+			, @Param("email") String email
+			, @Param("introduce") String introduce); 
+
+	// 가장 최근 사용자 정보 조회
+	public User selectLastUser();
+}
---
+++ src/main/java/com/mylb/spring/ex/jsp/service/UserService.java
@@ -0,0 +1,54 @@
+package com.mylb.spring.ex.jsp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mylb.spring.ex.jsp.domain.User;
+import com.mylb.spring.ex.jsp.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	// 사용자 정보를 전달 받고 저장하는 기능
+	public int addUser(
+			String name
+			, String birthday
+			, String email
+			, String introduce) {
+		
+		int count = userRepository.insertUser(name, birthday, email, introduce);
+		
+		return count;
+		
+	}
+	
+	// 가장 최근 등록된 사용자 정보 얻기
+	public User getLastUser() {
+		User user = userRepository.selectLastUser();
+		
+		return user;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
